200a5ece0806590d9485fd4daacb844d33c2ca9b
chenfushan <chenfushan1992@gmail.com>
2018-01-16 21:52:11 +0800

    delete file

--- simple-php/Controller/IndexController.class.php
+++ simple-php/Controller/IndexController.class.php
@@ -4,8 +4,6 @@
 	}
 	require_once dirname(__FILE__).'/Controller.class.php';
 	require_once dirname(__FILE__).'/../loadModules.php';
-	require_once dirname(__FILE__).'/../conf/CalculateConf.php';
-	require_once dirname(__FILE__).'/../biz/Rate.class.php';
 	/**
 	* index controller
 	*/
@@ -14,7 +12,6 @@
 		private static $rate;
 		function __construct()
 		{
-			self::$rate = new Rate();
 		}
 		/**
 		 * @param  string
@@ -24,51 +21,6 @@
 		public function indexController($name)
 		{
 			$this->ajaxReturn("request name is: ". $name);
-		}
-
-		public function getRate($pairs)
-		{
-			date_default_timezone_set('Asia/Shanghai');
-			$url = QUOTESHOST."pairs=".$pairs."&api_key=".APIKEY;
-			$res = file_get_contents($url);
-			$jsonRes = json_decode($res, true);
-			foreach ($jsonRes as $row) {
-				$res = self::$rate->updateRate($row);
-				if (!$res) {
-					Log::errorLog("update rate error.");
-					continue;
-				}
-			}
-			return true;
-		}
-
-		public function getPairListController()
-		{
-			$url = SYMBOLS."api_key=".APIKEY;
-			$res = file_get_contents($url);
-			$jsonRes = json_decode($res, true);
-			$pairs = "";
-			$index = 1;
-			foreach ($jsonRes as $row) {
-				if ($index % 5 == 0) {
-					$pairs .= $row;
-					$this->getRate($pairs);
-					$pairs = "";
-				}else{
-					$pairs = $pairs.$row.",";
-				}
-				$index++;
-			}
-			if ($pairs != "") {
-				$this->getRate($pairs);
-			}
-			
-			return true;
-		}
-
-		public function updateRateList()
-		{
-			
 		}
 	}
  ?>
