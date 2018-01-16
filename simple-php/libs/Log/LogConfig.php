ca4c3ea7c933376d21f4fbc6f0da743c32aff6d0
chenfushan <chenfushan1992@gmail.com>
2018-01-16 21:47:21 +0800

    fix bug

--- simple-php/libs/Log/LogConfig.php
+++ simple-php/libs/Log/LogConfig.php
@@ -4,4 +4,9 @@
 
 	//define the log path
 	define("LOCAL_LOGPATH" , dirname(__FILE__).'/../../Runtime/log/log_'.$time.'.log');
+	//define the access log path
+	define("ACCESS_LOGPATH", dirname(__FILE__).'/../../Runtime/log/access_'.$time.'.log');
+	//define the common error log path
+	define('ERROR_LOGPATH', dirname(__FILE__).'/../../Runtime/log/error_'.$time.'.log');;
+	
  ?>
